673dd851b28022ab515e2d4f467d436e3783bb4b
rafalpieniazek <rkpieniazek@gmail.com>
2017-08-13 10:59:31 +0200

    Initial commit

---
+++ src/main/java/pl/sda/hibernatetraining/HibernateTrainingApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.hibernatetraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateTrainingApplication.class, args);
+	}
+}
