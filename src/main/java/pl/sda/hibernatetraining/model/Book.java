b6a38f60aeda160f88bfcacc06bf6341b9820565
rafalpieniazek <rkpieniazek@gmail.com>
2017-08-13 11:16:33 +0200

    step1

---
+++ src/main/java/pl/sda/hibernatetraining/model/Book.java
@@ -0,0 +1,22 @@
+package pl.sda.hibernatetraining.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Book implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "book_year")
+    private int year;
+
+    // for hibernate
+    public Book() {
+    }
+}
