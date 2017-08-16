689e2d6920a9e45ace656725417c5c2a5e15404d
rafalpieniazek <rkpieniazek@gmail.com>
2017-08-16 19:59:05 +0200

    jdbc insert data with preparedStatement

--- src/main/java/pl/sda/hibernatetraining/JDBCExample.java
+++ src/main/java/pl/sda/hibernatetraining/JDBCExample.java
@@ -3,30 +3,46 @@ package pl.sda.hibernatetraining;
 import java.sql.*;
 
 public class JDBCExample {
-    static final String DB_URL = "jdbc:mysql://localhost:3307/LIBRARY";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/LIBRARY";
 
     static final String USER = "root";
-    static final String PASS = "root";
+    static final String PASS = "";
 
     public static void main(String[] args) {
+        String insertSql = "INSERT INTO  book ( title, book_year) VALUES  (?,?)";
+
         System.out.println("Connecting to database...");
         try (
                 Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                Statement stmt = conn.createStatement()
-        ) {
-            System.out.println("Creating statement...");
-            String sql = "SELECT id, title, book_year FROM book";
-            ResultSet rs = stmt.executeQuery(sql);
-            System.out.println("Extracting results ...");
+                Statement stmt = conn.createStatement();
+                PreparedStatement preparedStatemen = conn.prepareStatement(insertSql)
 
-            while (rs.next()) {
-                extractFromResultDate(rs);
-            }
-            rs.close();
+        ) {
+            insertMockBook(preparedStatemen);
+            selectAllBooks(stmt);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println("Goodbye!");
+    }
+
+    private static void selectAllBooks(Statement stmt) throws SQLException {
+        System.out.println("Selecting data ...");
+        String selectSql = "SELECT id, title, book_year FROM book";
+        ResultSet rs = stmt.executeQuery(selectSql);
+        System.out.println("Extracting results ...");
+        while (rs.next()) {
+            extractFromResultDate(rs);
+        }
+        rs.close();
+    }
+
+    private static void insertMockBook(PreparedStatement preparedStatemen) throws SQLException {
+        System.out.println("Inserting data");
+        preparedStatemen.setString(1, "Refactoring 3");
+        preparedStatemen.setInt(2, 2011);
+        preparedStatemen.executeUpdate();
     }
 
     private static void extractFromResultDate(ResultSet rs) throws SQLException {
